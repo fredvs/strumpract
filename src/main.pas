2f2637b27103473f5ecdf42dc89dcdfe9c3b00c9
fredvs <fiens@hotmail.com>
2023-02-27 01:15:43 +0100

    Change about-date to 2023!

--- src/main.pas
+++ src/main.pas
@@ -2088,7 +2088,7 @@ begin
     c_linefeed + c_linefeed +
     'Audio library: uos 1.8.0.' + c_linefeed +
     'https://github.com/fredvs/uos' + c_linefeed +
-    c_linefeed + '© 2022' + c_linefeed +
+    c_linefeed + '© 2023' + c_linefeed +
     'Fred van Stappen <fiens@hotmail.com>';
   aboutfo.Show(True);
 end;
